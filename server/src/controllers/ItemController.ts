d9d0a05fcf1ff80ce235c32aee4afdf6747d8e27
loiola0 <victorloiola@alu.ufc.br>
2020-06-05 02:54:11 -0300

    metade do mobile ja feito :)

--- server/src/controllers/ItemController.ts
+++ server/src/controllers/ItemController.ts
@@ -11,7 +11,7 @@ class ItemController {
       return {
          id: item.id,
          title: item.title,
-         image_url: `http://localhost:3333/uploads/${item.image}`,
+         image_url: `http://192.168.1.4:3333/uploads/${item.image}`,
       };
    });
 
